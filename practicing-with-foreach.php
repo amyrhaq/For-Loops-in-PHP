4cedabe27f19102d9b9d38410a74c9f5c2962895
Your Name Here <amyr@amyrhaq.com>
2013-09-25 15:54:50 -0700

    completed lesson 6/7

---
+++ practicing-with-foreach.php
@@ -0,0 +1,15 @@
+<!-- Complete the foreach loop in the editor by replacing with the correct loop syntax.  -->
+<html>
+<head>
+	<title>Practicing with ForEach</title>
+</head>
+<body>
+<?php 
+	$sentence = array("I'm ", "learning ", "PHP!");
+	foreach ($sentence as $word) 
+		{
+			echo $word;
+		}
+?>
+</body>
+</html>
