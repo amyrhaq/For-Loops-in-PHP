043dfbb8c7b758ca14a3931eb56642be2727ed05
Your Name Here <amyr@amyrhaq.com>
2013-09-25 14:19:56 -0700

    completed lesson 3/7

---
+++ writing-your-first-for-loop.php
@@ -0,0 +1,15 @@
+<!-- Go ahead and write a for loop that echos the numbers 10 up to and including 100, counting by 10s (e.g. 10, 20, 30...). -->
+<html>
+<head>
+	<title>Writing Your First For Loop</title>
+</head>
+<body>
+<?php
+$i = 10; 
+for ($i=10; $i < 110; $i=$i + 10) 
+	{ 
+	 	echo $i;
+	} 
+?>
+</body>
+</html>
