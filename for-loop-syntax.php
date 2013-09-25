bbcb10cb9c6ecefc630874435f8c072af80f6744
Your Name Here <amyr@amyrhaq.com>
2013-09-25 13:45:39 -0700

    completed lesson 2/7

---
+++ for-loop-syntax.php
@@ -0,0 +1,14 @@
+<!-- Complete the for loop in the editor by adding in the correct loop syntax.  -->
+<html>
+<head>
+	<title>For Loop Syntax</title>
+</head>
+<body>
+<?php 
+	for ($i= 2; $i < 11; $i=$i + 2)
+	{
+		echo $i;
+	} 
+?>
+</body>
+</html>
