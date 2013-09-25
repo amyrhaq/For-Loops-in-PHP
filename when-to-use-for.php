4789efc676d8fb2c15663d992b8ea3633eed7f08
Your Name Here <amyr@amyrhaq.com>
2013-09-25 15:29:33 -0700

    completed lesson 4/7

---
+++ when-to-use-for.php
@@ -0,0 +1,15 @@
+<!-- For now, let's get a little more practice in with for. Update your loop from the last exercise to make it count from 50 down to (and including) 5, counting by 5s. -->
+<html>
+<head>
+	<title>When to Use for</title>
+</head>
+<body>
+<?php
+$i = 50; 
+for ($i=50; $i > 0; $i=$i - 5) 
+	{ 
+	 	echo $i;
+	} 
+?>
+</body>
+</html>
